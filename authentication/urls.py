accb67cbe25ca50425fbff0059568b87c1cb1395
samimosmansabuj <samim.o.sabuj01@gmail.com>
2025-02-20 16:57:56 +0600

    authentication & creation api udpate

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+from .views import AdminCreationViews, AdminTokenObtainPairView, ClientRegistrationViews, ClientTokenObtainPairView
 
 urlpatterns = [
-    
+    path('auth/login/', ClientTokenObtainPairView.as_view(), name='auth-token'),
+    path('auth/registration/', ClientRegistrationViews.as_view(), name='client-registration'),
+    path('auth/admin-login/', AdminTokenObtainPairView.as_view(), name='admin-auth-token'),
+    path('auth/user-create/', AdminCreationViews.as_view(), name='admin-user-create'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import AdminTokenObtainPairSerializer, ClientTokenObtainPairSerializer, AdminUserCreationSerializers, ClientRegistrationSerializer
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView
+from .models import ClientModel, AdminModel
 
-# Create your views here.
+
+# =====Admin User Creation & Login Token views Start===
+class AdminTokenObtainPairView(TokenObtainPairView):
+    serializer_class = AdminTokenObtainPairSerializer
+
+class AdminCreationViews(CreateAPIView):
+    serializer_class = AdminUserCreationSerializers
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        AdminModel.objects.create(
+            user=user,
+            first_name = request.data['name']
+        ).save()
+        return Response(
+            {
+                'message': 'User Creation Successfully!'
+            }, status=status.HTTP_201_CREATED
+        )
+
+# =====Admin User Creation & Login Token views End===
+
+
+# =====Client User Creation & Login Token Views Start===
+class ClientTokenObtainPairView(TokenObtainPairView):
+    serializer_class = ClientTokenObtainPairSerializer
+
+class ClientRegistrationViews(CreateAPIView):
+    serializer_class = ClientRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        ClientModel.objects.create(
+            user = user,
+            first_name = request.data['first_name'],
+            last_name = request.data['last_name']
+        ).save()
+        return Response(
+            {
+                'message': 'Registration Successfully!'
+            }, status=status.HTTP_201_CREATED
+        )
+# =====Client User Creation & Login Token Views End===
+
+
+
