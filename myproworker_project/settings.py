db2b9b08911f02161e14c2961d0c8cba441838fa
samimosmansabuj <samim.o.sabuj01@gmail.com>
2025-03-09 14:51:34 +0600

    db migrate

--- myproworker_project/settings.py
+++ myproworker_project/settings.py
@@ -97,24 +97,24 @@ WSGI_APPLICATION = 'myproworker_project.wsgi.application'
 # ===================Database===============
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 # ===========================sqlite3=============================
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# ===========================PostgreSQL=============================
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST'),  # Use 'localhost' if running locally
-        'PORT': os.getenv('PORT'),  # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# # ===========================PostgreSQL=============================
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('NAME'),
+#         'USER': os.getenv('USER'),
+#         'PASSWORD': os.getenv('PASSWORD'),
+#         'HOST': os.getenv('HOST'),  # Use 'localhost' if running locally
+#         'PORT': os.getenv('PORT'),  # Default PostgreSQL port
+#     }
+# }
 
 
 
